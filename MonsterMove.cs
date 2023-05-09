1e2042e6783bf68cac1fdfd90fabe6924e33e762
chim-pan-zee <127918752+chim-pan-zee@users.noreply.github.com>
2023-05-09 10:49:02 +0900

    Create MonsterMove.cs

---
+++ MonsterMove.cs
@@ -0,0 +1,29 @@
+public class ZombieMale : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+}
